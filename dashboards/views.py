6aa9a76a1bde578c31a537946d9fe9663bb0dbfd
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-04-22 13:27:38 +0300

    Update views.py

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'dashboards/home.html')
+
+def dashboard_home_view(request):
+    return render(request, 'dashboards/dashboard_home.html')
+
+
+def teacher_dashboard_view(request):
+    return render(request, 'dashboards/teacher_dashboard.html')
+
+
+def student_dashboard_view(request):
+    return render(request, 'dashboards/student_dashboard.html')

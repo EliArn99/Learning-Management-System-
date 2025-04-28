71dd016ef41d5d02b4667730b6c6090946cfe5c4
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-04-28 11:47:47 +0300

    Update views.py

--- assignments/views.py
+++ assignments/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Assignment, Submission
+from .forms import AssignmentSubmissionForm
+from users.models import StudentProfile
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def assignment_list_view(request):
+    assignments = Assignment.objects.all().order_by('due_date')
+    return render(request, 'assignments/assignment_list.html', {'assignments': assignments})

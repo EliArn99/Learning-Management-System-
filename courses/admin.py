a66ae1acf4eaa72b2c2d856f276282413a093f0b
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-04-29 12:14:55 +0300

    Update admin.py

--- courses/admin.py
+++ courses/admin.py
@@ -7,7 +7,6 @@ class CourseAdmin(admin.ModelAdmin):
     search_fields = ('name', 'university_name')
     list_filter = ('university_name', 'teacher')
     prepopulated_fields = {'slug': ('name',)}
-    readonly_fields = ('slug',)
 
 
 @admin.register(Enrollment)
@@ -15,4 +14,3 @@ class EnrollmentAdmin(admin.ModelAdmin):
     list_display = ('student', 'course', 'enrolled_at', 'slug')
     search_fields = ('student__user__username', 'course__name')
     list_filter = ('enrolled_at',)
-    readonly_fields = ('slug',)

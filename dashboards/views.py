49a7e7232006cdf2db353fca10b0eac16158764f
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-04-29 12:16:42 +0300

    Update views.py

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,15 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from courses.models import Course
+from assignments.models import Assignment, Submission
 
 
 def home(request):
     return render(request, 'dashboards/home.html')
 
+
 def dashboard_home_view(request):
     return render(request, 'dashboards/dashboard_home.html')
 
 
+@login_required
 def teacher_dashboard_view(request):
-    return render(request, 'dashboards/teacher_dashboard.html')
+    teacher = request.user.teacherprofile
+    courses = Course.objects.filter(teacher=teacher)
+    submissions = Submission.objects.filter(assignment__course__in=courses)
+
+    return render(request, 'dashboards/teacher_dashboard.html', {
+        'courses': courses,
+        'submissions': submissions,
+    })
 
 
 @login_required

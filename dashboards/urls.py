6023c21489a32e68705611074f72420155f37ca4
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-05-19 12:03:51 +0300

    Add files via upload

---
+++ dashboards/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'dashboards'
+
+urlpatterns = [
+    path('', views.dashboard_home_view, name='dashboard_home'),
+    path('teacher/', views.teacher_dashboard_view, name='teacher_dashboard'),
+    path('about/', views.teacher_dashboard_view, name='about'),
+    path('student/', views.student_dashboard_view, name='student_dashboard'),
+]

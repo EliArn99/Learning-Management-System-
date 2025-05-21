5225f7210b4282507cba1cd87c47ebf7e3f42416
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-05-21 10:48:56 +0300

    Update urls.py

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -6,6 +6,7 @@ app_name = 'dashboards'
 urlpatterns = [
     path('', views.dashboard_home_view, name='dashboard_home'),
     path('teacher/', views.teacher_dashboard_view, name='teacher_dashboard'),
-    path('about/', views.teacher_dashboard_view, name='about'),
+    path('about/', views.about, name='about'),
+    path('contacts/', views.contacts, name='contacts'),
     path('student/', views.student_dashboard_view, name='student_dashboard'),
 ]

1a79dc4704bff675e336d16711f8ecf763b3bc47
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-05-25 13:09:51 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -17,11 +17,9 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile_view, name='profile'),
     path('profile/edit/', views.edit_profile_view, name='edit_profile'),
+    path('register/student/', views.register_student, name='register_student'),
+    path('register/teacher/', views.register_teacher, name='register_teacher'),
 
 ]
 
-#
-# urlpatterns += [
-#     path('login/', user_views.custom_login_view, name='login'),
-#     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-# ]
+

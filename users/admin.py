da8e2bcf32677294dd8db2460bf4f974f537d7aa
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-06-19 18:17:48 +0300

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,25 +1,49 @@
 from django.contrib import admin
-from .models import CustomUser, StudentProfile, TeacherProfile
+from .models import Student
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'enrollment_date')
+    search_fields = ('first_name', 'last_name', 'email')
+    list_filter = ('enrollment_date',)
+    ordering = ('-enrollment_date',)
+
+    readonly_fields = ('enrollment_date',)  # показва полето само за четене
+
+    fieldsets = (
+        (None, {
+            'fields': ('first_name', 'last_name', 'email', 'enrollment_date')
+        }),
+    )
 
 
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'is_student', 'is_teacher', 'is_superuser')
-    list_filter = ('is_student', 'is_teacher', 'is_superuser')
-    search_fields = ('username', 'email')
 
 
-@admin.register(StudentProfile)
-class StudentProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'age', 'is_approved')
-    list_filter = ('is_approved',)
-    search_fields = ('user__username',)
 
-@admin.register(TeacherProfile)
-class TeacherProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'is_approved')   #age', 'education',
-    list_filter = ('is_approved',)
-    search_fields = ('user__username',)
+
+
+# from django.contrib import admin
+# from .models import CustomUser, StudentProfile, TeacherProfile
+
+
+# @admin.register(CustomUser)
+# class CustomUserAdmin(admin.ModelAdmin):
+#     list_display = ('username', 'email', 'is_student', 'is_teacher', 'is_superuser')
+#     list_filter = ('is_student', 'is_teacher', 'is_superuser')
+#     search_fields = ('username', 'email')
+
+
+# @admin.register(StudentProfile)
+# class StudentProfileAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'age', 'is_approved')
+#     list_filter = ('is_approved',)
+#     search_fields = ('user__username',)
+
+# @admin.register(TeacherProfile)
+# class TeacherProfileAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'is_approved')   #age', 'education',
+#     list_filter = ('is_approved',)
+#     search_fields = ('user__username',)
 
 
 

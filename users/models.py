6feac8bdcfb670d400f637abca30d81a14ddb4a9
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-06-24 10:50:15 +0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,4 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class CustomUser(AbstractUser):
+    is_teacher = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=True)
+
 
 class Student(models.Model):
     first_name = models.CharField(max_length=50)
@@ -8,7 +15,6 @@ class Student(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-
 
 
 

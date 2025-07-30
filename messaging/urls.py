9c67690c0461c638816e1b70bd5d360ce2033ec2
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-07-30 15:30:50 +0300

    Update urls.py

--- messaging/urls.py
+++ messaging/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = 'messaging'
 
 urlpatterns = [
-    path('', views.messages_home, name='messages_home'),
+    path('', views.messages_home, name='home'),
     path('inbox/', views.inbox_view, name='inbox'),
     path('sent/', views.sent_view, name='sent'),
     path('send/', views.send_message_view, name='send_message'),
+    path('message/<int:pk>/', views.message_detail_view, name='message_detail'), # Нов път
 ]

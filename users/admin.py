9ebecdc2f4553a71eb536aa355a9e2306924c8a1
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-07-30 15:33:04 +0300

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,49 +1,44 @@
 from django.contrib import admin
-from .models import Student
+from .models import CustomUser, StudentProfile, TeacherProfile
 
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'enrollment_date')
-    search_fields = ('first_name', 'last_name', 'email')
-    list_filter = ('enrollment_date',)
-    ordering = ('-enrollment_date',)
 
-    readonly_fields = ('enrollment_date',)  # показва полето само за четене
-
-    fieldsets = (
-        (None, {
-            'fields': ('first_name', 'last_name', 'email', 'enrollment_date')
-        }),
-    )
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_student', 'is_teacher', 'is_superuser', 'is_active')
+    list_filter = ('is_student', 'is_teacher', 'is_superuser', 'is_active') # Added is_active to filter
+    search_fields = ('username', 'email')
 
 
 
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'age', 'faculty_number', 'is_approved')
+    list_filter = ('is_approved',)
+    search_fields = ('user__username', 'faculty_number')
+    actions = ['approve_students']
+
+    def approve_students(self, request, queryset):
+        updated_count = queryset.update(is_approved=True)
+        self.message_user(request, f"{updated_count} студентски профил/а беше/ха одобрен/и успешно.")
+    approve_students.short_description = "Одобри избрани студентски профили"
 
 
+@admin.register(TeacherProfile)
+class TeacherProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'education', 'experience_years', 'is_approved')
+    list_filter = ('is_approved',)
+    search_fields = ('user__username', 'education')
+    actions = ['approve_teachers']
+
+    def approve_teachers(self, request, queryset):
+        """Action to set selected teacher profiles to is_approved=True."""
+        updated_count = queryset.update(is_approved=True)
+        self.message_user(request, f"{updated_count} учителски профил/а беше/ха одобрен/и успешно.")
+    approve_teachers.short_description = "Одобри избрани учителски профили"
 
 
-# from django.contrib import admin
-# from .models import CustomUser, StudentProfile, TeacherProfile
-
-
-# @admin.register(CustomUser)
-# class CustomUserAdmin(admin.ModelAdmin):
-#     list_display = ('username', 'email', 'is_student', 'is_teacher', 'is_superuser')
-#     list_filter = ('is_student', 'is_teacher', 'is_superuser')
-#     search_fields = ('username', 'email')
-
-
-# @admin.register(StudentProfile)
-# class StudentProfileAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'age', 'is_approved')
-#     list_filter = ('is_approved',)
-#     search_fields = ('user__username',)
-
-# @admin.register(TeacherProfile)
-# class TeacherProfileAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'is_approved')   #age', 'education',
-#     list_filter = ('is_approved',)
-#     search_fields = ('user__username',)
-
-
-
+# If you plan to use RegistrationCode later, this is fine
+# @admin.register(RegistrationCode)
+# class RegistrationCodeAdmin(admin.ModelAdmin):
+#     list_display = ('code', 'is_used', 'user', 'created_at')
+#     search_fields = ('code',)

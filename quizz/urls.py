bb009a13334ec04379669920ca7cc95cb98faa77
Eli Arnaytska <eli_arnaytska@abv.bg>
2025-10-14 16:13:22 +0300

    Update urls.py

--- quizz/urls.py
+++ quizz/urls.py
@@ -3,7 +3,6 @@ from .views import (
     QuizListView, QuizCreateView, QuizDetailView,
     QuizTakeView, quiz_results, QuizQuestionsUpdateView, QuestionUpdateView
 )
-from . import views
 
 
 app_name = 'quizz'

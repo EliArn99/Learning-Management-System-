e5d7241a3e8092400d24a0ce666eea09b5835552
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-01-28 15:56:32 +0200

    Update admin.py

--- courses/admin.py
+++ courses/admin.py
@@ -1,31 +1,31 @@
 from django.contrib import admin
 from .models import Course, Enrollment, Module, ModuleCategory
 
+
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('name', 'university_name', 'teacher', 'slug')
-    search_fields = ('name', 'university_name', 'teacher__user__username')
-    list_filter = ('university_name', 'teacher')
-    prepopulated_fields = {'slug': ('name',)}
+    list_display = ("name", "university_name", "teacher", "price", "slug")
+    search_fields = ("name", "university_name", "teacher__user__username")
+    list_filter = ("university_name", "teacher")
+    prepopulated_fields = {"slug": ("name",)}
 
 
 @admin.register(Enrollment)
 class EnrollmentAdmin(admin.ModelAdmin):
-    list_display = ('course', 'student', 'enrolled_at', 'slug')
-    search_fields = ('course__name', 'student__user__username')
-    list_filter = ('course', 'enrolled_at')
-    readonly_fields = ('enrolled_at',)
-    prepopulated_fields = {'slug': ('student', 'course')}
+    list_display = ("course", "student", "enrolled_at", "is_paid", "transaction_id")
+    search_fields = ("course__name", "student__user__username", "transaction_id")
+    list_filter = ("course", "is_paid", "enrolled_at")
+    readonly_fields = ("enrolled_at", "transaction_id")
 
 
 @admin.register(Module)
 class ModuleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'code', 'course', 'category')
-    search_fields = ('title', 'code', 'course__name')
-    list_filter = ('category', 'course')
+    list_display = ("title", "code", "course", "category")
+    search_fields = ("title", "code", "course__name")
+    list_filter = ("category", "course")
 
 
 @admin.register(ModuleCategory)
 class ModuleCategoryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
+    list_display = ("name",)
+    search_fields = ("name",)

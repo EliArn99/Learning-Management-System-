01e50ad6c6769290955a45d3c5ae29a2fe3f47a4
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-02-05 17:48:16 +0200

    Update urls.py

--- messaging/urls.py
+++ messaging/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = 'messaging'
+app_name = "messaging"
 
 urlpatterns = [
-    path('', views.messages_home, name='home'),
-    path('inbox/', views.inbox_view, name='inbox'),
-    path('sent/', views.sent_view, name='sent'),
-    path('send/', views.send_message_view, name='send_message'),
-    path('message/<int:pk>/', views.message_detail_view, name='message_detail'), # Нов път
+    path("", views.messages_home, name="home"),
+    path("inbox/", views.inbox_view, name="inbox"),
+    path("sent/", views.sent_view, name="sent"),
+    path("send/", views.send_message_view, name="send_message"),
+    path("message/<int:pk>/", views.message_detail_view, name="message_detail"),
 ]

88e87511065480269196a3b58d8ada57d103e7c7
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-05-15 18:43:32 +0300

    Update signals.py

--- users/signals.py
+++ users/signals.py
@@ -1,12 +1,30 @@
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
-# from .models import CustomUser, StudentProfile, TeacherProfile
-#
-#
-# @receiver(post_save, sender=CustomUser)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         if instance.is_staff:
-#             StudentProfile.objects.create(user=instance)
-#         elif instance.is_teacher:
-#             TeacherProfile.objects.create(user=instance)
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import CustomUser, StudentProfile, TeacherProfile
+
+
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    if not created:
+        return
+
+    if instance.is_student:
+        StudentProfile.objects.get_or_create(
+            user=instance,
+            defaults={
+                "age": 18,
+                "is_approved": False,
+            },
+        )
+
+    elif instance.is_teacher:
+        TeacherProfile.objects.get_or_create(
+            user=instance,
+            defaults={
+                "age": 25,
+                "education": "",
+                "experience_years": 0,
+                "is_approved": False,
+            },
+        )

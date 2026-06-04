1d4b826106348661f1c8387ebefef235635b5bd2
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-06-04 17:30:01 +0300

    Update urls.py

--- mini_lms/urls.py
+++ mini_lms/urls.py
@@ -1,20 +1,27 @@
-
-from django.contrib import admin
-from django.urls import path, include
-from dashboards.views import home
-# from users.views import login_view
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+from dashboards.views import home
+
 urlpatterns = [
     path("", home, name="home"),
+
     path("admin/", admin.site.urls),
+
     path("courses/", include("courses.urls")),
     path("assignments/", include("assignments.urls")),
     path("dashboard/", include("dashboards.urls")),
     path("messages/", include("messaging.urls")),
     path("users/", include("users.urls")),
-    path("quizz/", include("quizz.urls", namespace="quizz")),
+
+    # quiz вместо quizz
+    path("quiz/", include("quizz.urls", namespace="quizz")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
